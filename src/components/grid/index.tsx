8a68c40498f7acf96f98c0d27eb24862b34e6361
amberlu00 <amberlu00@gmail.com>
2024-01-19 14:24:27 -0600

    Filter and hover WIP

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import LazyLoad from "react-lazy-load";
 import data from "../../data/tiles.json";
 import Lightbox, { Image } from "../lightbox";
-import LazyLoad from "react-lazy-load";
+
 
 function Grid() {
     const [images, setImages] = useState<Array<Image>>([]);
 
     const [size, setSize] = useState<number>(0);
+
+    const [showFilters, setShowFilters] = useState(false);
 
     const [lightBox, setLightBox] = useState<Image | null>();
 
@@ -29,11 +32,25 @@ function Grid() {
         setSize(data.length);
     }, []);
 
+    const onFilterChange = () => {
+        setShowFilters(!showFilters);
+    };
+
     return (
         <section className="flex min-h-screen flex-col items-center justify-between p-8">
             <h4 className="font-semibold text-5xl pb-5">commissions by koi</h4>
             <div className="justify-start flex items-center gap-5">
-                {/* <button>filters</button> */}
+                <button
+                    onClick={() => onFilterChange()}
+                    className="hover:underline"
+                >
+                    filters
+                </button>
+                {showFilters && (
+                    <div className="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                        hi
+                    </div>
+                )}
                 <button onClick={() => onRandom()} className="hover:underline">
                     surprise me!
                 </button>
@@ -41,15 +58,15 @@ function Grid() {
             <div className="justify-start flex items-center gap-5 pb-5">
                 <p>{size} commissions displayed</p>
             </div>
-            <div className="columns-4 space-y-5 gap-4 place-items-start">
+            <div className="2xl:columns-5 lg:columns-4 md:columns-3 sm:columns-1 space-y-5 gap-4 place-items-start">
                 {images.map((image) => {
                     return (
-                        <LazyLoad offset={500} key={`${image.id}`}>
+                        <LazyLoad offset={200} key={`${image.id}`}>
                             <div
                                 className="flex h-min w-full object-cover"
                                 onClick={() => onClick(image)}
                             >
-                                <img src={image.url} />
+                                <img src={image.url} loading="lazy" />
                             </div>
                         </LazyLoad>
                     );
